581028aee2d981d6140f196c76a23f07dbb2576a
sadvikareddypalle <sadvikareddy333@gmail.com>
2026-08-28 22:47:08 +0530

    Apply bug fix patch

--- src/main/java/SEGroup/MavenJavaProject/App.java
+++ src/main/java/SEGroup/MavenJavaProject/App.java
@@ -5,6 +5,6 @@ package SEGroup.MavenJavaProject;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+    	System.out.println("Hello World! Welcome to Maven");;
     }
 }
